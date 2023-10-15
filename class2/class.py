a0a92f3f1921a2c0f462842e663bacc53a727e07
gordon20121224 <john@example.com>
2023-10-15 09:50:25 +0800

    class3

---
+++ class2/class.py
@@ -0,0 +1,28 @@
+a = max(14, 80, 90, 400)
+print(a)
+
+# 取最大值
+# max()函數可以取最大值
+
+# 取得字串長度
+print(len("dddddddddddd"))
+print(len("gordon1224555"))
+
+# 取的資料物件的型態
+print(type(True))
+print(type("hello"))
+
+# 將輸入數值轉換為整屬型態
+print(int(False))
+print(int(True))
+
+# 將輸入數值轉換為浮點數型態
+print(float(123))
+print(float(True))
+
+# 將輸入數值轉換為布林型態
+print(bool(1))
+print(bool("hello"))
+
+# 將輸入數值轉換為字串型態
+print(str(True))
