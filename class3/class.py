ab14ed0fbd342ae33b5b022d0f8e9034e5baeb03
gordon20121224 <gordongg67@gmail.com>
2023-10-15 11:01:57 +0800

    class3

---
+++ class3/class.py
@@ -0,0 +1,27 @@
+# name = 'Gordon'
+# age = 10
+# new_str_1 = 'i am %s,this year%d' % (name, age)
+# print(new_str_1)
+# try:
+#     num = int(input('add a number'))
+#     total = num + 1
+#     print(total)
+
+# except:
+#     print('error')
+
+# !=not equal
+# == equal
+# >=greater equal
+# <=smaller equal
+# > greater
+# <smaller
+# not = opsite
+# and = equal true or False
+# or = if one true equal to True
+
+password = input('enter the number')
+if password == "1224":
+    print('welcome to home')
+else:
+    print('error, you are died')
