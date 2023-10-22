ea677bb6bee18d7b6d67fbe5cdb1fa86a025a83f
gordon20121224 <gordongg67@gmail.com>
2023-10-22 10:59:18 +0800

    class4

---
+++ class4/class.py
@@ -0,0 +1,52 @@
+# password = input('entering the pasword')
+# if password == "1234":
+#     print("welcome gordon")
+# elif password == "5678":
+#     print("welcome stranger")
+# else: print("error")
+
+# score = int(input("entering the score:"))
+
+# if score > 90:
+#     print("A")
+# elif score >= 80 and score <= 89:
+#     print("B")
+# elif score >= 70 and score <= 79:
+#     print("C")
+# elif score >= 60 and score <= 69:
+#     print("D")
+# else:
+#     print("E")
+# number = int(input("enter a number:"))
+# if number % 2 == 0:
+# print(str(number) + "is odd ")
+# else:
+# print(str(number) + "is even")
+
+import turtle
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.done()
+# v
+# turtle.done()
+
+# turtle.color("blue")
+# turtle.shape("circle")
+
+# turtle.stamp()
+# turtle.penup
+
+turtle.color("blue")
+turtle.shape("circle")
+turtle.penup()
+for i in range(0, 360, 2):
+    turtle.forward(i)
+    turtle.stamp()
+    turtle.right(18)
+turtle.done()
---
+++ class4/hw.py
@@ -0,0 +1,4 @@
+w = int(input("輸入體重: "))
+h = float(input("輸入身高: "))
+bmi = w / (h * h)
+print(bmi)
