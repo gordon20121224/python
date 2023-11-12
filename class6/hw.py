ca2ed8ffb2f135fe517ab1a9486130a2297dc386
gordon20121224 <gordongg67@gmail.com>
2023-11-12 09:31:37 +0800

    class6

--- class6/hw.py
+++ class6/hw.py
@@ -1,14 +1,14 @@
-sum = input("please enter a num:")
-print(sum)
-sum = int(sum)
-for i in range(1, sum):
-    if i % 3 == 0 or i % 7 == 0:
-        print(i)
+# sum = input("please enter a num:")
+# print(sum)
+# sum = int(sum)
+# for i in range(1, sum):
+#     if i % 3 == 0 or i % 7 == 0:
+#         print(i)
 
+n - int(input("enter num:"))
 
-# a = input(please enter a number:)
-# for i in range(1,2):
-#     >>>val= '*' * 3
-#     >>>print(val)
-#     >>>***
-#     if a =
+for i in range(n):
+    print(i)
+    print(" " * (n - 1 - i) + "* " * (i * 2 + 1))
+for i in range(n):
+    print(" " * (n - 1) + "*")
