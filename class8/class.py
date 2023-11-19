19b5e73f7cc025fcd6ee873949d1b02baad4ef69
gordon20121224 <gordongg67@gmail.com>
2023-11-19 11:00:58 +0800

    class8

---
+++ class8/class.py
@@ -0,0 +1,35 @@
+# weather =["晴天", "多雲", "雨天", "晴天", "多雲", "雷陣雨", "晴天"]
+# print(weather)
+# while True :
+#     try:
+#         a = int(input("enter a date to fixx(1 to 7):"))
+#     except:
+#        print("error")
+#        continue
+#     if a > len(weather) or a < 1 :
+#         print("error")
+#     else:
+#         print("u fix the date is" + str(a))
+#         print("the original date is" + weather[a - 1] )
+#         new_weather = input("please enter the new weather:")
+#         weather[a - 1] = new_weather
+#         print("the fixed weather is " + weathera - 1 ])
+#         print(weather)
+#         break
+
+# l = ["a", "b" ,"c"]
+# a = l.copy()
+# a[0] = 1
+# print(a, l)
+        
+# a = ["milk, bread "]
+# b = ["apple, banana"]
+# print(a + b)
+fruit_list = ["火龍果","哈密瓜","百香果","橘子","蘋果" ,「香蕉」,「梨」,「李」,「桃]
+         
+print("the longet fruit is " + max(fruit_list))
+print("the shortest fruit is" + min (fruit_list))
+
+list(abc)
+list ([4,5,6])
+list (range(3ssssssssss))
