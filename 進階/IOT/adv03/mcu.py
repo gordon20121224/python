66b24aa358d02b4b48e679dc04d4ce9780fd24a0
gordon20121224 <gordongg67@gmail.com>
2024-03-17 10:53:49 +0800

    adv03

---
+++ 進階/IOT/adv03/mcu.py
@@ -0,0 +1,58 @@
+class gpio:
+    def __init__(self):
+        self._D0 = 16
+        self._D1 = 5
+        self._D2 = 4
+        self._D3 = 0
+        self._D4 = 2
+        self._D5 = 14
+        self._D6 = 12
+        self._D7 = 13
+        self._D8 = 15
+        self._SDD3 = 10
+        self._SDD2 = 9
+
+
+@property
+def D0(self):
+    return self._D0
+
+
+def D1(self):
+    return self._D1
+
+
+def D2(self):
+    return self._D2
+
+
+def D3(self):
+    return self._D3
+
+
+def D4(self):
+    return self._D4
+
+
+def D5(self):
+    return self._D5
+
+
+def D6(self):
+    return self._D6
+
+
+def D7(self):
+    return self._D7
+
+
+def D8(self):
+    return self._D8
+
+
+def SDD3(self):
+    return self._SDD3
+
+
+def SDD2(self):
+    return self._SDD2
